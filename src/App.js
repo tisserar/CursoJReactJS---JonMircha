b1337f07713667de43dbba91a48af87fe61fcc44
Rodrigo Tissera <rodrigo.tissera@gmail.com>
2022-11-24 23:33:48 -0300

    clase 10  Eventos & Property Initializers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
-import Eventos from "./components/Eventos";
+import {EventosES6,EventosES7} from "./components/Eventos";
 
 function App() {
 
@@ -47,7 +47,9 @@ function App() {
         <hr/>
         <RenderizadoElementos/>
         <hr/>
-        <Eventos/>
+        <EventosES6/>
+        <hr/>
+        <EventosES7/>
       </section>
       </header>
     </div>
--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,6 @@
     import React, { Component } from "react";
     
-    export default class Eventos extends Component{
+    export class EventosES6 extends Component{
     constructor(props){
         super(props);
         this.state = {
@@ -28,7 +28,7 @@
     render (){
         return(
             <div>
-                <h2>Eventos en componente de clase</h2>
+                <h2>Eventos en componente de clase ES6</h2>
                 <nav>
                   <button onClick={this.sumar}>+</button>
                   <button  onClick={this.restar}>-</button>
@@ -37,4 +37,41 @@
             </div>
         );
     }
+    }
+
+    //properties inicializer
+    export class EventosES7 extends Component{
+          state = {
+              contador: 0,
+          };
+      
+
+      //Arrow Function
+      sumar =(e) =>{
+        console.log("Sumando")
+        console.log(this);
+        this.setState({
+          contador:this.state.contador + 1,
+        });
+      }
+      restar = (e) =>{
+        console.log("Restando")
+        console.log(this);
+        this.setState({
+          contador:this.state.contador - 1,
+        });
+      }
+  
+      render (){
+          return(
+              <div>
+                  <h2>Eventos en componente de clase ES7</h2>
+                  <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button  onClick={this.restar}>-</button>
+                  </nav>
+                  <h3>{this.state.contador}</h3>
+              </div>
+          );
+      }
     }
