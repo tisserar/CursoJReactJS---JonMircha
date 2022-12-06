dd286b83f0bc92b36626c8643e266726efe62614
Rodrigo Tissera <rodrigo.tissera@gmail.com>
2022-12-06 08:21:31 -0300

    clase 13 ciclo de vida de componentes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
 import {EventosES6,EventosES7,MasSobreEventos} from "./components/Eventos";
 import ComunicacionComponentes from "./components/ComunicacionComponentes";
+import CicloVida from "./components/CicloVida";
 
 function App() {
 
@@ -55,6 +56,8 @@ function App() {
         <MasSobreEventos/>
         <hr/>
         <ComunicacionComponentes/>
+        <hr/>
+        <CicloVida/>
       </section>
       </header>
     </div>
