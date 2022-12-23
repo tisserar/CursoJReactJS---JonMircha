784d08b10ce037e3d3597896ccd3e3ee05c13556
Rodrigo Tissera <rodrigo.tissera@gmail.com>
2022-12-23 00:15:16 -0300

    clase 16 Hooks: useState

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {EventosES6,EventosES7,MasSobreEventos} from "./components/Eventos";
 import ComunicacionComponentes from "./components/ComunicacionComponentes";
 import CicloVida from "./components/CicloVida";
 import AjaxApis from "./components/AjaxApis";
+import ContadorHooks from "./components/ContadorHooks";
 
 function App() {
 
@@ -61,6 +62,8 @@ function App() {
         <CicloVida/>
         <hr/>
         <AjaxApis/>
+        <hr/>
+        <ContadorHooks titulo="Seguidores"/>
       </section>
       </header>
     </div>
