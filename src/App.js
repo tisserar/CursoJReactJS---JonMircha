bf9db1c0d3442b18eea95b05ffa95d9936abc566
Rodrigo Tissera <rodrigo.tissera@gmail.com>
2023-01-13 07:42:32 -0300

    clase 20 Hooks Personalizados

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ContadorHooks from "./components/ContadorHooks";
 import ScrollHooks from "./components/ScrollHooks";
 import RelojHooks from "./components/RelojHooks";
 import AjaxHooks from "./components/AjaxHooks";
+import HooksPersonalizados from "./components/HooksPersonalizados";
 
 
 function App() {
@@ -74,6 +75,8 @@ function App() {
         <RelojHooks/>
         <hr/>
         <AjaxHooks/>
+        <hr/>
+        <HooksPersonalizados/>
       </section>
       </header>
     </div>
