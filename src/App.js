8022a8e46ea7bdbf85667e75553e107444458bcb
Rodrigo Tissera <rodrigo.tissera@gmail.com>
2023-01-20 00:00:04 -0300

    clase 21 Referenicas

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ScrollHooks from "./components/ScrollHooks";
 import RelojHooks from "./components/RelojHooks";
 import AjaxHooks from "./components/AjaxHooks";
 import HooksPersonalizados from "./components/HooksPersonalizados";
+import Referencias from "./components/Referencias";
 
 
 function App() {
@@ -77,6 +78,8 @@ function App() {
         <AjaxHooks/>
         <hr/>
         <HooksPersonalizados/>
+        <hr/>
+        <Referencias/>
       </section>
       </header>
     </div>
